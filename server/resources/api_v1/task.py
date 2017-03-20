f505d429ae8552d62144ef375d8e4cad9a65b568
imhuwq <imhuwq@gmail.com>
2017-03-21 01:02:48 +0800

    user task relationship database migration

--- server/resources/api_v1/task.py
+++ server/resources/api_v1/task.py
@@ -1,7 +1,9 @@
 from server.resources.api_v1 import apiv1, BaseHandler
 
-from server.models.celery import PeriodicTask as Task
 from server.models.celery import TaskRole
+from server.models.celery import PeriodicTask as Task
+
+from server.application.tornado_handler import login_required, Argument
 
 task = apiv1.create_resource('task', prefix='/tasks')
 
@@ -9,6 +11,7 @@ task = apiv1.create_resource('task', prefix='/tasks')
 # test task api is accessible
 @task.route('/')
 class TaskIndexResource(BaseHandler):
+    @login_required
     def get(self):
         tasks = self.query(Task).filter_by(_role=TaskRole.INITIALIZER.value).all()
         task_count = len(tasks)
